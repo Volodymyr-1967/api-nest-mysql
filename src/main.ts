6b0e5b4cd30fd87956183a4447b0657df538a06b
Vladimir Rybalchenko <vladimire51@gmail.com>
2022-01-03 10:02:39 +0200

    commit with port 3300

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3300);
+  const port = 3300;
+  await app.listen(port, () => {
+    console.log(`\n` + `Server is running on http://localhost:${port}`);
+  });
 }
 bootstrap();

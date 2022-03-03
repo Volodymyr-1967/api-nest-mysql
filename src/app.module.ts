8573723db3b5423af3c505613d60491b8b7c5333
Vladimir Rybalchenko <vladimire51@gmail.com>
2022-03-03 15:49:00 +0200

    commit without add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import db_config from './config/db_config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [db_config],
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql', // process.env.DB_TYPE as DatabaseType,
+      type: 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: Number.parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
--- src/config/db_config.ts
+++
@@ -1,8 +0,0 @@
-export default () => ({
-  db_type: process.env.DB_TYPE || 'mysql',
-  db_host: process.env.DB_HOST || 'localhost',
-  db_port: process.env.DB_PORT || 3306,
-  db_username: process.env.DB_USERNAME || 'root',
-  db_password: process.env.DB_PASSWORD || 'market565',
-  db_database: process.env.DB_DATABASE || 'mydbase',
-});
